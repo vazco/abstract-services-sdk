25635a09bf8edcda3a050dcec97e665b71083d06
cristo-rabani <cristo.rabani@gmail.com>
2016-12-08 16:47:21 +0100

    async gets services

--- src/lib/index.js
+++ src/lib/index.js
@@ -9,6 +9,7 @@ export class AbstractServicesSDK {
         this._publicKeyString = publicKeyString;
         this._serviceClasses = ServiceClasses;
         this._services = {};
+        this._tokenGetters = {};
     }
 
     generateWebToken ({userId = '', groupId = '', sessionId = '', hash = '', ttl = 86400} = {}) {
@@ -49,7 +50,32 @@ export class AbstractServicesSDK {
         return result.cipher;
     }
 
-    getService (name) {
+    /**
+     * Sets token for authentication, token you can generate from SDK.generateWebToken
+     * @param {string|function} token or token getter, which returns promise.
+     * @param {string} serviceName sets token for given service or for all as a default.
+     * @returns {undefined}
+     */
+    setToken (token, serviceName = 'default') {
+        if (typeof token === 'function') {
+            this._tokenGetters[serviceName] = token;
+            return;
+        }
+        this._tokenGetters[serviceName] = () => token;
+        if (this._services) {
+            Object.keys(this._services).forEach(name => this._updateTokenForService(name));
+        }
+    }
+
+    _updateTokenForService (name) {
+        if (this._tokenGetters[name]) {
+            this._services[name].setToken(this._tokenGetters[name]);
+        } else if (this._tokenGetters['default']) {
+            this._services[name].setToken(this._tokenGetters['default']);
+        }
+    }
+
+    async getService (name) {
         if (!this._services[name]) {
             if (!this._serviceClasses[name]) {
                 throw new Error('Missing service under name: '+name);
@@ -60,6 +86,7 @@ export class AbstractServicesSDK {
                 baseURL: this._url
             });
         }
+        this._updateTokenForService(name);
         return this._services[name];
     }
 }
